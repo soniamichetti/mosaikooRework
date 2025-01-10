1a25d01b660db306935f07a13fc72a972e51c079
soniamichetti <156791064+soniamichetti@users.noreply.github.com>
2025-01-10 10:39:15 +0100

    Delete Mosaikoo Rework v2 directory

--- Mosaikoo Rework v2/deconnexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start() ;
-unset($_SESSION['admin']) ;
-unset($_SESSION['autorisation']) ;
-header("location: index.php") ;
-?>
--- Mosaikoo Rework v2/rappel.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"D:\laragon\www\derniereVersion\Mosaikoo Rework v2\php.exe" "D:\laragon\www\derniereVersion\Mosaikoo Rework v2\rappel.php"
